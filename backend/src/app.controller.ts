624c604edc7604dfe41084009f92e613e95fdda5
aring <1303340995@qq.com>
2022-03-08 16:15:18 +0800

    feat: 新增上传文件接口

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Post, UploadedFile, UseInterceptors, Headers } from '@nestjs/common'
+import { FileInterceptor } from '@nestjs/platform-express'
+import { createWriteStream } from 'fs'
+import { join } from 'path'
+import { UserService } from 'src/user/user.service'
+import { suc, fail } from 'src/utils/response'
+import { ApiResponse, ApiTags, ApiOperation, ApiParam } from '@nestjs/swagger'
+import { UploadResDTO } from './utils/app.dto'
 
+@ApiTags('公共')
 @Controller()
-export class AppController {}
+export class AppController {
+  constructor(private readonly userService: UserService) {}
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiOperation({ summary: '上传文件' })
+  @ApiParam({ name: 'file', example: '(文件)', description: '文件' })
+  @ApiResponse({ status: 0, type: UploadResDTO })
+  async upload(@Headers('token') token: string, @UploadedFile() file: Express.Multer.File) {
+    if ((await this.userService.vaildAuth({ token })) !== 1) return fail('您没有权限')
+    if (!file) return fail('未接收到文件')
+    const fileName = `${Date.now()}-${file.originalname}`
+    const writeFile = createWriteStream(join('C:', 'upload-files', fileName))
+    writeFile.write(file.buffer)
+    return suc(
+      {
+        fileName,
+        fileUrl: `http://81.68.189.158:86/${fileName}`,
+        fileSize: file.size
+      },
+      '上传成功'
+    )
+  }
+}
---
+++ backend/src/utils/app.dto.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty, IsOptional, IsEnum, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger'
+import { BaseResDTO, BasePageDTO } from 'src/utils/base.dto';
+
+class UploadDTO {
+  @ApiProperty({ example: '1646726623796-图片名.png', description: '文件名' })
+  fileName: string
+
+  @ApiProperty({ example: 'http://81.68.189.158:86/1646726623796-图片名.png', description: '文件地址' })
+  fileUrl: string
+
+  @ApiProperty({ example: 1200, description: '文件大小' })
+  fileSize: number
+}
+
+export class UploadResDTO extends BaseResDTO {
+  @ApiProperty({ type: UploadDTO, description: '文件信息' })
+  data?: UploadDTO
+}
